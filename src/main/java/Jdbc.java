f2de85d9408dfa6a473a459e00d4c86c966d2c96
chandrakalasv <chandrakalasv1502@gmail.com>
2022-07-18 15:13:37 +0530

    added messeage on master branch

---
+++ src/main/java/Jdbc.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Jdbc {
+    public static void main(String[] args) {
+        System.out.println("employee pay roll service jdbc");
+    }
+}
+
+
