5ac09efe663f9318923c5a90b55a3555fc93ffb2
chandrakalasv <chandrakalasv1502@gmail.com>
2022-07-18 15:34:43 +0530

    added code to create employee_payroll table in payroll_service database

--- src/main/java/Jdbc.java
+++ src/main/java/Jdbc.java
@@ -9,12 +9,16 @@ public class Jdbc {
         try {
 //            com.mysql.jdbc.Driver;
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://root@localhost:3306/", "root", "Maanvij@123");
+                    "jdbc:mysql://root@localhost:3306/payroll_service", "root", "Maanvij@123");
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("show databases");
-            while (rs.next()) {
-                System.out.println(rs.getString(1));
-            }
+            String sql = "CREATE TABLE employee_payroll " +
+                    "(id int unsigned not null auto_increment, " +
+                    " name VARCHAR(255), " +
+                    " salary Double, " +
+                    " Start_Date date, " +
+                    " PRIMARY KEY ( id ))";
+            stmt.executeUpdate(sql);
+            System.out.println("Created table in given database...");
             con.close();
         } catch (Exception e) {
             System.out.println(e);
