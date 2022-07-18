37f1f3782b3c5a2f0ef3e6f3b4068e0890336928
chandrakalasv <chandrakalasv1502@gmail.com>
2022-07-18 15:16:48 +0530

    added code to create, show and use payroll service database

--- src/main/java/Jdbc.java
+++ src/main/java/Jdbc.java
@@ -6,6 +6,19 @@ import java.sql.Statement;
 public class Jdbc {
     public static void main(String[] args) {
         System.out.println("employee pay roll service jdbc");
+        try {
+//            com.mysql.jdbc.Driver;
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://root@localhost:3306/", "root", "Maanvij@123");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("show databases");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
 
